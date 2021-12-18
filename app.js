5b2b0b3499a16f64a49ae79101eb4d7d068d36d6
Altan Ali Ejder <altanalie@gmail.com>
2021-12-18 22:45:04 +0300

    added authentication and category model

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
 const pageRoute = require('./routes/pageRoute');
 const courseRoute = require('./routes/courseRoute');
+const categoryRoute = require('./routes/categoryRoute');
+const userRoute = require('./routes/userRoute');
+
 const app = express();
 
 mongoose.connect('mongodb://localhost/smartedu-db').then(() => {
@@ -10,12 +15,28 @@ mongoose.connect('mongodb://localhost/smartedu-db').then(() => {
 
 app.set('view engine', 'ejs');
 
+global.userIN = null;
+
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: 'my_keyboard_cat',
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({ mongoUrl: 'mongodb://localhost/smartedu-db' }),
+  })
+);
+app.use('*', (req, res, next) => {
+  userIN = req.session.userID;
+  next();
+});
 
 app.use('/', pageRoute);
 app.use('/courses', courseRoute);
+app.use('/categories', categoryRoute);
+app.use('/users', userRoute);
 
 const port = 3000;
 app.listen(port, () => {
