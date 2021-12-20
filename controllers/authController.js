4b513e34c6f4fe433bbe4315ec64a220a1c5eec9
Altan Ali Ejder <altanalie@gmail.com>
2021-12-20 20:53:17 +0300

    course operations added

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,13 +1,15 @@
-const User = require('../models/User');
 const bcrypt = require('bcrypt');
+const User = require('../models/User');
+const Category = require('../models/Category');
 
 exports.createUser = async (req, res) => {
   try {
     const user = await User.create(req.body);
-    res.status(201).json({
-      status: 'success',
-      user,
-    });
+    res.status(201).redirect('/login');
+    // .json({
+    //   status: 'success',
+    //   user,
+    // })
   } catch (error) {
     res.status(400).json({
       status: 'fail',
@@ -23,7 +25,7 @@ exports.loginUser = async (req, res) => {
       if (same) {
         req.session.userID = user._id;
 
-        res.status(200).redirect('/');
+        res.status(200).redirect('/users/dashboard');
       }
     });
   } catch (error) {
@@ -38,4 +40,12 @@ exports.logOutUser = (req, res) => {
   req.session.destroy(() => {
     res.redirect('/');
   });
+};
+
+exports.getDashboardPage = async (req, res) => {
+  const user = await User.findOne({ _id: req.session.userID });
+  const categories = await Category.find();
+  res
+    .status(200)
+    .render('dashboard', { page_name: 'dashboard', user, categories });
 };
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const authController = require('../controllers/authController');
-
+const authMiddleware = require('../middlewares/authMiddleware');
 const router = express.Router();
 
 router.route('/signup').post(authController.createUser);
 router.route('/login').post(authController.loginUser);
 router.route('/logout').get(authController.logOutUser);
-
+router.route('/dashboard').get(authMiddleware, authController.getDashboardPage);
 module.exports = router;
